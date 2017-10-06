72e8e9b8409cf5f2d8985be0907e5d0aefe22791
devopssamples <32574418+devopssamples@users.noreply.github.com>
2017-10-06 13:23:57 -0400

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo git clone https://github.com/testcomcast/comcastsamples.git /home/ubuntu/comcastsamples
-sudo chef-client --local-mode -c /home/ubuntu/comcastsamples/chefconfig/client.rb --override-runlist nginx::deployapp
+cd /home/ubuntu
+sudo git clone https://github.com/devopssamples/nginxcluster.git 
+sudo chef-client --local-mode -c /home/ubuntu/nginxcluster/nginxsamples/chefconfig/client.rb --override-runlist nginx::deployapp
