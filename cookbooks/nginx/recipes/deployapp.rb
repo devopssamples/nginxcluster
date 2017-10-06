6fdfaa103dc6b64b0699ecff645069bef7120707
devopssamples <32574418+devopssamples@users.noreply.github.com>
2017-10-06 13:19:46 -0400

    Update deployapp.rb

--- cookbooks/nginx/recipes/deployapp.rb
+++ cookbooks/nginx/recipes/deployapp.rb
@@ -1,7 +1,4 @@
-#execute 'gitclone' do
-#  command "sudo git clone https://github.com/testcomcast/comcastsamples.git /home/ubuntu/comcastsamples"
-#end
+
 execute 'deployapp' do
-  #command "cp /home/ubuntu/comcastapp/nginxapp/index.html /var/www/html/index.html"
   command "sudo chmod 755 /home/ubuntu/comcastsamples/deploy/deploy.sh; /home/ubuntu/comcastsamples/deploy/deploy.sh"
 end
